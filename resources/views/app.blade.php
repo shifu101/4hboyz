cab98dff423848f4e61550a554000bd61381d141
Amos Billy <amosbillykipchumba@gmail.com>
2025-02-09 19:52:57 +0300

    primeicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,7 +31,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('fonts/primeicons.css') }}">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap">
         <!-- Scripts -->
         @routes
