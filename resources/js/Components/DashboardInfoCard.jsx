65853f3d172cbddb4e5d54dd66cb7ca9a52b2e4d
Amos Billy <amosbillykipchumba@gmail.com>
2025-04-12 02:54:13 +0300

    changes

--- resources/js/Components/DashboardInfoCard.jsx
+++ resources/js/Components/DashboardInfoCard.jsx
@@ -6,20 +6,22 @@ export default function DashboardInfoCard({ title, value, icon = '', iconColor =
     const iconBackgroundClass = `flex align-items-center justify-content-center bg-${iconColor}-100 border-round`;
 
     return (
-        <Link href={to} className="col-12 lg:col-6 xl:col-3">
-            <div className="card mb-0">
+        <Link href={to} className="col-12 lg:col-6 xl:col-3 min-h-full">
+            <div className="card mb-0 h-full flex flex-col justify-between">
                 <div className="flex justify-content-between mb-3">
                     <div>
                         <span className="block text-500 font-medium mb-3">{title}</span>
                         <div className="text-900 font-medium text-xl">{value}</div>
                     </div>
                     <div className={iconBackgroundClass}
-                         style={{width: '2.5rem', height: '2.5rem'}}>
+                        style={{ width: '2.5rem', height: '2.5rem' }}>
                         <i className={iconClass} />
                     </div>
                 </div>
-                <span className="text-green-500 font-medium">{ descriptionValue}</span>
-                <span className="text-500"> {descriptionText}</span>
+                <div>
+                    <span className="text-green-500 font-medium">{descriptionValue}</span>
+                    <span className="text-500"> {descriptionText}</span>
+                </div>
             </div>
         </Link>
     );
